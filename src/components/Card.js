106f1e951d14d144353521d6bbaa46e52b7d673a
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-01-10 23:26:21 -0700

    added more cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,23 @@
 import "../styles/Card.scss"
 
-function Card(){
+function Card({image,header,summary,tag,readTime,id}){
+    console.log(header)
     return(
         <div className="card">
             {/* image */}
             <div className="image-container">
-            <img src="https://img.redbull.com/images/c_crop,x_333,y_0,h_2003,w_2671/c_fill,w_480,h_358/q_auto,f_auto/redbullcom/2022/12/19/ndpy1wrtqgmj9vqzasgi/red-bull-magnitude" alt="redbull photot" className="image" />
+            <img src={image} alt="redbull photo" className="image" />
             </div>
             {/* title */}
             {/* paragraph */}
             {/* bottom left- genre or redbull bulletin logo */}
             {/* bottom right- time to read */}
             <div className="body-container">
-            <p className="header">7 best surf competitions in the world</p>
-            <p className="summary"> From massive barrel riding to big waves to airs in a pool, there are as...</p>
+            <p className="header">{header}</p>
+            <p className="summary">{summary}</p>
             <div className="bottom">
-            <p className="tag">SURFING</p>
-            <p className="read-Time">7 min read</p>
+            <p className="tag">{tag}</p>
+            <p className="read-Time">{readTime}</p>
             </div>
             </div>
         </div>
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,21 @@
 import "../styles/Content.scss"
 import Card from "./Card.js"
+import CardData from "../data/cardData.js"
+
 
 function Content(){
+    CardData.map((data)=>console.log(data.id))
     return(<div className="content">
         <div className="title">Discover the World of Red Bull</div>
         {/* create card component */}
-        <Card />
+        {CardData.map((data)=> <Card 
+                                image={data.image} 
+                                header={data.header} 
+                                summary={data.summary}
+                                tag={data.tag}
+                                readTime={data.readTime}
+                                id={data.id}
+                                 />)}
         {/* load more button */}
     </div>
     )
