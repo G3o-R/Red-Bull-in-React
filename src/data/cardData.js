0466da87a80f59ddde5e368f6cfdb885bafeaf28
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-01-10 22:49:39 -0700

    added to the dataBase

--- src/data/cardData.js
+++ src/data/cardData.js
@@ -1,86 +1,98 @@
 CardData = [
     {
         image: "https://img.redbull.com/images/c_crop,x_500,y_0,h_2003,w_2504/c_fill,w_550,h_448/q_auto,f_auto/redbullcom/2022/12/19/ndpy1wrtqgmj9vqzasgi/red-bull-magnitude",
-        header: "",
-        tag: "",
-        readTime: "",
+        header: "7 best surf competitions in the world",
+        paragraph:"From massive barrel riding to big waves to airs in a pool, there are as many other surf competitions in the world as there are surfing styles.",
+        tag: "SURFING",
+        readTime: "7 min read",
         id:1
     },
     {
         image: "",
         header: "",
+        paragraph: "",
         tag: "",
         readTime: "",
         id:2
     },
     {
         image: "https://img.redbull.com/images/c_crop,x_864,y_0,h_5208,w_6944/c_fill,w_550,h_400/q_auto,f_auto/redbullcom/2022/10/10/qrgghjradl1gukbq6tcw/danny-macaskill-golden-gate-bridge",
-        header: "",
-        tag: "",
-        readTime: "",
+        header: "Danny MacAskill reveals the inside story of Postcard from San Francisco",
+        paragraph: "Discover how Danny visualizes his lines using his famous notepad, and try some of them for yourself as interactive games. ",
+        tag: "MTB",
+        readTime: "12 min read",
         id:3
     },
     {
         image: "https://img.redbull.com/images/c_crop,x_0,y_0,h_5464,w_7285/c_fill,w_550,h_400/q_auto,f_auto/redbullcom/2022/12/5/p5jhcyeqmzdqrz74fl4y/rail-balance-danny-macaskill-danny-in-san-francisco",
-        header: "",
-        tag: "",
-        readTime: "",
+        header: "6 of the best bike trials videos you can watch right now",
+        paragraph: "Looking to get a hit of trials action? You're in the right place as we feature a selection of videos from the best proponents and stars of the mountain bike discipline.",
+        tag: "TRIAL BIKING",
+        readTime: "4 min read",
         id:4
     },
     {
         image: "",
         header: "",
+        paragraph: "",
         tag: "",
         readTime: "",
         id:5
     },
     {
         image: "https://img.redbull.com/images/c_crop,x_0,y_0,h_1680,w_2239/c_fill,w_550,h_400/q_auto,f_auto/redbullcom/2022/11/9/ga1sbyvdg8ccqx9hpgco/rick-ross",
-        header: "",
-        tag: "",
-        readTime: "",
+        header: "Red Bull Symphonic Atlanta recap: Rick Ross showed us why he's the boss",
+        paragraph: "Rick Ross and Orchestra Noir gave the sold out crowd at the Atlanta Symphony Hall a night to remember.",
+        tag: "MUSIC",
+        readTime: "4 min read",
         id:6
     },
     {
         image: "https://img.redbull.com/images/c_crop,x_0,y_552,h_1960,w_2450/c_fill,w_550,h_447/q_auto,f_auto/redbullcom/2022/1/27/vdaim6zunouvvhwwxzhh/jaylen-brown-portrait",
-        header: "",
-        tag: "",
-        readTime: "",
+        header: "Jaylen Brown has his Eye on the Ball",
+        paragraph: "Jaylen Brown is an All-Star forward for the Boston Celtics, and off the court, his powerful voice makes him a natural leader and advocate for change.",
+        tag: "https://www.redbull.com/v3/resources/images/client/trb-logo-dark-v2.svg", //red bull bulletin
+        readTime: "19 min read",
         id:7
     },
     {
         image: "https://img.redbull.com/images/c_crop,x_258,y_0,h_1495,w_1993/c_fill,w_550,h_400/q_auto,f_auto/redbullcom/2022/11/21/qzzk3jw9x4ovdcjlvmhl/project-19",
-        header: "",
-        tag: "",
-        readTime: "",
+        header: "Amy Chmelecki jumps at the chance for new world record with Project 19",
+        paragraph: "Combining her love of skydiving and social activism to encourage women and their need to vote, Amy Chmelecki attempts to break a world record.",
+        tag: "SKYDIVING",
+        readTime: "7 min read",
         id:8
     },
     {
         image: "https://img.redbull.com/images/c_crop,x_320,y_0,h_2000,w_2667/c_fill,w_550,h_400/q_auto,f_auto/redbullcom/2022/11/13/zjrua5d9oi99wgupr47r/red-bull-bc-one-world-final-belt-2022-india",
-        header: "",
-        tag: "",
-        readTime: "",
+        header: "Victor and India on winning Red Bull BC One in New York City",
+        paragraph: "Find out what the 2022 Red Bull BC One World Final winners, B-Girl India and B-Boy Victor, had to say about their historic victories in New York. ",
+        tag: "BREAKING",
+        readTime: "6 min read",
         id:9
     },
     {
         image: "https://img.redbull.com/images/c_crop,x_23,y_0,h_4000,w_5333/c_fill,w_550,h_400/q_auto,f_auto/redbullcom/2022/11/7/eckaiqzxcqfc7xzdvabj/gabriella-angotti-jones",
-        header: "",
-        tag: "",
-        readTime: "",
+        header: "In focus with surf photographer and journalist Gabriella Angotti-Jones ",
+        paragraph: "Gabriella stopped surfing at a young age because she didn’t feel comfortable on the water. Now, she’s taking it all back and more with a new book that looks at Black surfers and mental health.",
+        tag: "SURFING",
+        readTime: "6 min read",
         id:10
     },
     {
         image: "https://img.redbull.com/images/c_crop,x_292,y_0,h_3675,w_4900/c_fill,w_550,h_400/q_auto,f_auto/redbullcom/2022/10/22/ixzuouytgu0uqfx9m54j/red-bull-rampage-finals-2022-podiun",
-        header: "",
-        tag: "",
-        readTime: "",
+        header: "Brett Rheeder crowned two-time Red Bull Rampage champion ",
+        paragraph: "The world's best freeride mountain bikers demonstrated the sport's progression on historic grounds. ",
+        tag: "MTB",
+        readTime: "8 min read",
         id:11
     },
     {
         image: "https://img.redbull.com/images/c_crop,x_1309,y_260,h_1067,w_1333/c_fill,w_550,h_447/q_auto,f_auto/redbullcom/2022/9/21/ckxctc54a2khebwffeb1/brandon-semenuk-mtb-rallyracing",
-        header: "",
-        tag: "",
-        readTime: "",
+        header: "Brandon Semenuk: Portrait of an Artist",
+        paragraph: "Step inside the creative, deliberate and private mind of the world’s best mountain biker.",
+        tag: "https://www.redbull.com/v3/resources/images/client/trb-logo-dark-v2.svg",//red bull bulletin
+        readTime: "23 min read",
         id:12
     },
 
