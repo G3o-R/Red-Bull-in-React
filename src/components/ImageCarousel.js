38d0f1093f6de2ab3fe11224445f9f6b37b7f513
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-01-22 19:10:03 -0700

    added media queries

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -4,10 +4,12 @@ function ImageCarousel(){
     return(
             <div className="container">
         <div className="imageCarousel">
-            <img class="img" src="https://img.redbull.com/images/c_crop,x_0,y_249,h_1600,w_4000/c_fill,w_1920,h_768/q_auto,f_auto/redbullcom/2022/12/19/ekjjmprxxjg331pztsu3/red-bull-scrable-series" alt="scramble series"/>
-            {/* <h2 className="event">OFFROAD</h2>
+            <img className="img" src="https://img.redbull.com/images/c_crop,x_0,y_249,h_1600,w_4000/c_fill,w_1920,h_768/q_auto,f_auto/redbullcom/2022/12/19/ekjjmprxxjg331pztsu3/red-bull-scrable-series" alt="scramble series"/>
+            {/* <div className="carousel-content">
+            <h2 className="event">OFFROAD</h2>
             <h2 className="title"> Scramble series returns for an action-packed season</h2>
-            <button className="btn">View Event Calandar</button> */}
+            <button className="btn">View Event Calandar</button>
+            </div> */}
             </div>
         </div>
     )
